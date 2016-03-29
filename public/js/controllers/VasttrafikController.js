399ca9aecbb88079ce254654bc28d727d082dd67
anderssonjohn <johnand@student.chalmers.se>
2016-03-29 23:19:15 +0200

    Add frontend for vasttrafik

--- public/js/controllers/VasttrafikController.js
+++ public/js/controllers/VasttrafikController.js
@@ -1,7 +1,14 @@
 app.controller('VasttrafikController', function($scope, $http) {
-    $http.get('/api/vasttrafik')
+    $http.get('/api/vasttrafik-chalmers')
         .success(function(data) {
-            $scope.departures = data;
+            $scope.departures_chalmers = data.DepartureBoard.Departure;
+        })
+        .error(function(data) {
+            console.log('Error:' + data);
+        });
+    $http.get('/api/vasttrafik-tvargatan')
+        .success(function(data) {
+            $scope.departures_chalmers = data;
         })
         .error(function(data) {
             console.log('Error:' + data);
