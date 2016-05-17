656a2906cbdb99d60e4f2577d0cf6e4067c0841f
hjorthjort <rikard.hjort@gmail.com>
2016-05-17 21:20:42 +0200

    Pages controller with index and new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-get 'welcome' => 'pages#home'
+root 'pages#index'
+resources :pages
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
